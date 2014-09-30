064e7a9d54fc0b235981598c429a1163e1b7dcee
konradhalas <halas.konrad@gmail.com>
2014-09-30 17:34:20 +0200

    Fix template path. Add TravisCI status image.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # djoser
 
+[![Build Status](https://travis-ci.org/sunscrapers/djoser.svg?branch=master)](https://travis-ci.org/sunscrapers/djoser)
+
 Set of Django Rest Framework views to handle users authentication.
--- testproject/settings.py
+++ testproject/settings.py
@@ -1,3 +1,7 @@
+import os
+
+BASE_DIR = os.path.dirname(__file__)
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -19,5 +23,5 @@ INSTALLED_APPS = (
 )
 
 TEMPLATE_DIRS = (
-    'templates',
+    os.path.join(BASE_DIR, 'templates'),
 )
