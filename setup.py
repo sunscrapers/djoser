e42eab184d0b9f7f2ae55c5edf027ceb8873a788
konradhalas <halas.konrad@gmail.com>
2014-10-01 12:39:34 +0200

    Copy templates during installation.

--- setup.py
+++ setup.py
@@ -6,10 +6,13 @@ setup(
     name='djoser',
     packages=['djoser'],
     install_requires=[
-        'Django>=1.7',
-        'djangorestframework>=2.4.3',
+       'Django>=1.7',
+       'djangorestframework>=2.4.3',
     ],
     tests_require=[
         'djet>=0.0.9'
-    ]
+    ],
+    package_data={
+        'templates': ['templates/*.html', 'templates/*.txt']
+    },
 )
