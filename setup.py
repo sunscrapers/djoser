254ead8080c6b47a077a654b3ebc9d8ac51e7040
konradhalas <halas.konrad@gmail.com>
2014-10-06 14:35:21 +0200

    Fix setup.py.

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ setup(
     name='djoser',
     packages=['djoser'],
     install_requires=[
-#       'Django>=1.7',
-#       'djangorestframework>=2.4.3',
+      'Django>=1.7',
+      'djangorestframework>=2.4.3',
     ],
     tests_require=[
-#        'djet>=0.0.9'
+       'djet>=0.0.9'
     ],
     include_package_data=True,
     zip_safe=False,
