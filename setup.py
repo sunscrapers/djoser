d16fadb40a7b45107d4367a831bb8058f8e5c0b4
konradhalas <halas.konrad@gmail.com>
2014-10-07 10:42:19 +0200

    Update info about package.

--- setup.py
+++ setup.py
@@ -5,8 +5,13 @@ from setuptools import setup
 setup(
     name='djoser',
     packages=['djoser'],
+    license='MIT',
+    author='SUNSCRAPERS',
+    description='REST version of Django authentication system.',
+    author_email='admin@sunscrapers.com',
+    long_description=open('README.md').read(),
     install_requires=[
-      'Django>=1.7',
+      'Django>=1.5',
       'djangorestframework>=2.4.3',
     ],
     tests_require=[
