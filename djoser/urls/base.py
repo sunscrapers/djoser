190449bfe3bbf0c6360eda633d94b4cd18f087c5
ashishmohite <mohite.aashish.1@gmail.com>
2015-12-26 18:00:18 +0530

    RemovedInDjango110Warning: django.conf.urls.patterns() is deprecated and will be removed in Django 1.10 #101
    - resolve the warning
    - remove pylint errors from urls files

--- djoser/urls/base.py
+++ djoser/urls/base.py
@@ -1,17 +1,56 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from djoser import views
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-base_urlpatterns = patterns('',
-    url(r'^me/$', views.UserView.as_view(), name='user'),
-    url(r'^register/$', views.RegistrationView.as_view(), name='register'),
-    url(r'^activate/$', views.ActivationView.as_view(), name='activate'),
-    url(r'^{0}/$'.format(User.USERNAME_FIELD), views.SetUsernameView.as_view(), name='set_username'),
-    url(r'^password/$', views.SetPasswordView.as_view(), name='set_password'),
-    url(r'^password/reset/$', views.PasswordResetView.as_view(), name='password_reset'),
-    url(r'^password/reset/confirm/$', views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-)
+base_urlpatterns = [
+    url(
+        r'^me/$',
+        views.UserView.as_view(),
+        name='user'
+    ),
 
-urlpatterns = base_urlpatterns + patterns('', url(r'^$', views.RootView.as_view(), name='root'))
+    url(
+        r'^register/$',
+        views.RegistrationView.as_view(),
+        name='register'
+    ),
+
+    url(
+        r'^activate/$',
+        views.ActivationView.as_view(),
+        name='activate'
+    ),
+
+    url(
+        r'^{0}/$'.format(User.USERNAME_FIELD),
+        views.SetUsernameView.as_view(),
+        name='set_username'
+    ),
+
+    url(
+        r'^password/$',
+        views.SetPasswordView.as_view(),
+        name='set_password'
+    ),
+
+    url(
+        r'^password/reset/$',
+        views.PasswordResetView.as_view(),
+        name='password_reset'
+    ),
+    url(
+        r'^password/reset/confirm/$',
+        views.PasswordResetConfirmView.as_view(),
+        name='password_reset_confirm'
+    ),
+]
+
+urlpatterns = base_urlpatterns + [
+    url(
+        r'^$',
+        views.RootView.as_view(),
+        name='root'
+    )
+]
