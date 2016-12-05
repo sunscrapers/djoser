3b86d65ac05af23744bd26f115cd3e79242840af
Pawel Swiecki <p.swiecki@sunscrapers.com>
2016-12-05 15:56:35 +0100

    Bump package version to 0.2.2.1.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ REQUIREMENTS = [i.strip() for i in open('requirements.txt').readlines()]
 
 setup(
     name='djoser',
-    version='0.2.1',
+    version='0.2.2.1',
     packages=find_packages(include=('djoser*',)),
     license='MIT',
     author='SUNSCRAPERS',
