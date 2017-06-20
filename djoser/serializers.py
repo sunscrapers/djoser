6cc6d2da4ea7a5400ac51aae4339fd08df833159
Kamil Niski <kamil.niski@solution4future.com>
2017-06-20 17:15:46 +0200

    Add six.iteritems to dict for loop in SerializersManager

--- djoser/serializers.py
+++ djoser/serializers.py
@@ -244,7 +244,7 @@ class TokenSerializer(serializers.ModelSerializer):
 class SerializersManager(object):
     def __init__(self, serializer_confs):
         self.serializers = {}
-        for serializer_name, serializer in serializer_confs.copy().iteritems():
+        for serializer_name, serializer in six.iteritems(serializer_confs.copy()):
             if isinstance(serializer, six.string_types):
                 serializer = import_string(serializer)
             self.serializers[serializer_name] = serializer
