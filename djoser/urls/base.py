dc0da77f83722296f953155796edb63547f4d78b
Piotr Szpetkowski <piotr.szpetkowski@pyquest.space>
2017-08-18 14:09:48 +0200

    Fix base urls formatting

--- djoser/urls/base.py
+++ djoser/urls/base.py
@@ -8,10 +8,24 @@ base_urlpatterns = (
     url(r'^me/$', views.UserView.as_view(), name='user'),
     url(r'^register/$', views.RegistrationView.as_view(), name='register'),
     url(r'^activate/$', views.ActivationView.as_view(), name='activate'),
-    url(r'^{0}/$'.format(User.USERNAME_FIELD), views.SetUsernameView.as_view(), name='set_username'),
+    url(
+        r'^{0}/$'.format(User.USERNAME_FIELD),
+        views.SetUsernameView.as_view(),
+        name='set_username'
+    ),
     url(r'^password/$', views.SetPasswordView.as_view(), name='set_password'),
-    url(r'^password/reset/$', views.PasswordResetView.as_view(), name='password_reset'),
-    url(r'^password/reset/confirm/$', views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    url(
+        r'^password/reset/$',
+        views.PasswordResetView.as_view(),
+        name='password_reset'
+    ),
+    url(
+        r'^password/reset/confirm/$',
+        views.PasswordResetConfirmView.as_view(),
+        name='password_reset_confirm'
+    ),
 )
 
-urlpatterns = base_urlpatterns + (url(r'^$', views.RootView.as_view(), name='root'),)
+urlpatterns = base_urlpatterns + (
+    url(r'^$', views.RootView.as_view(), name='root'),
+)
