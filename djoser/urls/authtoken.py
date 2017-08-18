34b583d8c841a620b368d5a5cc05659a00707a52
Piotr Szpetkowski <piotr.szpetkowski@pyquest.space>
2017-08-18 14:09:21 +0200

    Fix authtoken urls formatting

--- djoser/urls/authtoken.py
+++ djoser/urls/authtoken.py
@@ -5,5 +5,9 @@ from . import base
 urlpatterns = base.base_urlpatterns + (
     url(r'^login/$', views.LoginView.as_view(), name='login'),
     url(r'^logout/$', views.LogoutView.as_view(), name='logout'),
-    url(r'^$', views.RootView.as_view(urls_extra_mapping={'login': 'login', 'logout': 'logout'}), name='root'),
+    url(
+        r'^$', views.RootView.as_view(urls_extra_mapping={
+            'login': 'login', 'logout': 'logout'
+        }), name='root'
+    ),
 )
