09baaa6144fc8e8c2ca84ae8ddcdf3c3cfb1ccf7
Piotr Szpetkowski <piotr.szpetkowski@pyquest.space>
2017-09-25 16:21:18 +0200

    Use trailing slashes on URLs for consistence

--- djoser/urls/authtoken.py
+++ djoser/urls/authtoken.py
@@ -4,6 +4,6 @@ from djoser import views
 
 
 urlpatterns = [
-    url(r'^token/create$', views.TokenCreateView.as_view(), name='login'),
-    url(r'^token/destroy$', views.TokenDestroyView.as_view(), name='logout'),
+    url(r'^token/create/$', views.TokenCreateView.as_view(), name='login'),
+    url(r'^token/destroy/$', views.TokenDestroyView.as_view(), name='logout'),
 ]
