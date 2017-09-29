cfd3e7b18dcbfdd678a0524d97e8ef0fb41cbc81
Piotr Szpetkowski <piotr.szpetkowski@pyquest.space>
2017-09-29 19:08:00 +0200

    Simplify JWT URL patterns

--- djoser/urls/jwt.py
+++ djoser/urls/jwt.py
@@ -4,7 +4,7 @@ from rest_framework_jwt import views
 
 
 urlpatterns = [
-    url(r'^jwt-token/create/', views.obtain_jwt_token, name='jwt-create'),
-    url(r'^jwt-token/refresh/', views.refresh_jwt_token, name='jwt-refresh'),
-    url(r'^jwt-token/verify/', views.verify_jwt_token, name='jwt-verify'),
+    url(r'^jwt/create/', views.obtain_jwt_token, name='jwt-create'),
+    url(r'^jwt/refresh/', views.refresh_jwt_token, name='jwt-refresh'),
+    url(r'^jwt/verify/', views.verify_jwt_token, name='jwt-verify'),
 ]
