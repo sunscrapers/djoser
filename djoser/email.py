1e1b84c8966ec6ea7a237a30d223ffcabe67f485
haxoza <prze.lewandowski@gmail.com>
2017-10-19 22:44:40 +0200

    Fix wrong url for password reset email. See #237.

--- djoser/email.py
+++ djoser/email.py
@@ -32,5 +32,5 @@ class PasswordResetEmail(BaseEmailMessage):
         user = context.get('user')
         context['uid'] = utils.encode_uid(user.pk)
         context['token'] = default_token_generator.make_token(user)
-        context['url'] = settings.ACTIVATION_URL.format(**context)
+        context['url'] = settings.PASSWORD_RESET_CONFIRM_URL.format(**context)
         return context
