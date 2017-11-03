a148dd0a2e1147bdec91a85de3a1d0c6297af5a6
Piotr Szpetkowski <piotr.szpetkowski@pyquest.space>
2017-11-03 18:00:04 +0100

    Add facebook authentication tests
    Add JWT token strategy obtain test
    Update ProviderAuthSerializer to create user on validate
    Remove redundant fields from ProviderAuthSerializer

--- djoser/social/serializers.py
+++ djoser/social/serializers.py
@@ -7,10 +7,6 @@ from djoser.conf import settings
 
 
 class ProviderAuthSerializer(serializers.Serializer):
-    # GET authorization URL
-    id = serializers.IntegerField(read_only=True)
-    email = serializers.CharField(read_only=True)
-
     # GET auth token
     token = serializers.CharField(read_only=True)
     user = serializers.CharField(read_only=True)
@@ -20,18 +16,8 @@ class ProviderAuthSerializer(serializers.Serializer):
     state = serializers.CharField(required=False, write_only=True)
 
     def create(self, validated_data):
-        strategy = load_strategy(self.context['request'])
-        redirect_uri = strategy.session_get('redirect_uri')
-
-        backend_name = self.context['view'].kwargs['provider']
-        backend = load_backend(
-            strategy, backend_name, redirect_uri=redirect_uri
-        )
-        user = backend.auth_complete()
+        user = validated_data['user']
         return settings.SOCIAL_AUTH_TOKEN_STRATEGY.obtain(user)
-
-    def update(self, instance, validated_data):
-        pass
 
     def validate_state(self, value):
         strategy = load_strategy(self.context['request'])
@@ -46,3 +32,20 @@ class ProviderAuthSerializer(serializers.Serializer):
             backend.validate_state()
         except exceptions.AuthException:
             raise serializers.ValidationError('State could not be verified.')
+
+    def validate(self, attrs):
+        strategy = load_strategy(self.context['request'])
+        redirect_uri = strategy.session_get('redirect_uri')
+
+        backend_name = self.context['view'].kwargs['provider']
+        backend = load_backend(
+            strategy, backend_name, redirect_uri=redirect_uri
+        )
+
+        try:
+            user = backend.auth_complete()
+        except exceptions.AuthException:
+            raise serializers.ValidationError(
+                'Failed to finish authentication.'
+            )
+        return {'user': user}
