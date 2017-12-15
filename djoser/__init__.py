db9fe4d720a052de65a739d9c72278e3e096a873
Piotr Szpetkowski <piotr.szpetkowski@pyquest.space>
2017-12-15 13:12:46 +0100

    Add project metada to __init__.py

---
+++ djoser/__init__.py
@@ -0,0 +1,8 @@
+__title__ = 'Djoser'
+__version__ = '2.0.0'
+__author__ = 'Sunscrapers'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2014-2017 Sunscrapers'
+
+# Version synonym
+VERSION = __version__
