c66e76f78d1665ad08c668a74cd64456a13394ac
Piotr Szpetkowski <piotr.szpetkowski@pyquest.space>
2018-01-15 18:58:06 +0100

    Add password validator definitions used in tests

---
+++ tests/validators.py
@@ -0,0 +1,7 @@
+from django.core.exceptions import ValidationError
+
+
+class Is666(object):
+    def validate(self, password, *args, **kwargs):
+        if password == '666':
+            raise ValidationError("Password 666 is not allowed.")
