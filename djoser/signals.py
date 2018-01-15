36f043e1eeacb86072cc7ca38343420cf5c9cefc
Piotr Szpetkowski <piotr.szpetkowski@pyquest.space>
2018-01-15 19:24:39 +0100

    Add and update signals for pipelines

--- djoser/signals.py
+++ djoser/signals.py
@@ -1,8 +1,20 @@
 from django.dispatch import Signal
 
 
-# New user has registered.
-user_registered = Signal(providing_args=["user", "request"])
+# User has updated password
+password_updated = Signal(providing_args=['user', 'request'])
 
-# User has activated his or her account.
-user_activated = Signal(providing_args=["user", "request"])
+# New user has registered
+user_created = Signal(providing_args=['user', 'request'])
+
+# User has updated account
+user_updated = Signal(providing_args=['user', 'request'])
+
+# User has removed account
+user_deleted = Signal(providing_args=['user', 'request'])
+
+# User has activated account
+user_activated = Signal(providing_args=['user', 'request'])
+
+# User has updated username
+username_updated = Signal(providing_args=['user', 'request'])
