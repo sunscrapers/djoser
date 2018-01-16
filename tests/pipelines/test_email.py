3002b7810baf7715516226968bf1ed94ee782120
Piotr Szpetkowski <piotr.szpetkowski@pyquest.space>
2018-01-16 15:31:22 +0100

    Remove password reset tests from email tests

--- tests/pipelines/test_email.py
+++ tests/pipelines/test_email.py
@@ -51,28 +51,3 @@ def test_failed_confirmation_email_user_empty_context():
     context = {}
     with pytest.raises(AssertionError):
         pipelines.email.confirmation_email(request=None, context=context)
-
-
-def test_valid_password_reset_email_without_request(test_user, mailoutbox):
-    context = {'user': test_user}
-    assert test_user.is_active is True
-
-    pipelines.email.password_reset_email(request=None, context=context)
-    test_user.refresh_from_db()
-
-    assert len(mailoutbox) == 1
-    assert mailoutbox[0].to == [test_user.email]
-    assert test_user.is_active is True
-
-
-def test_failed_password_reset_email_user_without_email(test_user, mailoutbox):
-    test_user.email = ''
-    context = {'user': test_user}
-    pipelines.email.password_reset_email(request=None, context=context)
-    assert len(mailoutbox) == 0
-
-
-def test_failed_password_reset_email_user_empty_context():
-    context = {}
-    with pytest.raises(AssertionError):
-        pipelines.email.password_reset_email(request=None, context=context)
