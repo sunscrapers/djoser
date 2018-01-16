af48b7f2c0d30c4e656b6591291337b14acb41ba
Piotr Szpetkowski <piotr.szpetkowski@pyquest.space>
2018-01-16 15:33:17 +0100

    Remove password reset email from email pipeline

--- djoser/pipelines/email.py
+++ djoser/pipelines/email.py
@@ -26,13 +26,3 @@ def confirmation_email(request, context):
     assert user_email is not None
     to = [user_email]
     settings.EMAIL.confirmation(request, context).send(to)
-
-
-def password_reset_email(request, context):
-    utils.validate_context_user_for_email(context)
-    user = context['user']
-
-    user_email = utils.get_user_email(user)
-    assert user_email is not None
-    to = [user_email]
-    settings.EMAIL.password_reset(request, context).send(to)
