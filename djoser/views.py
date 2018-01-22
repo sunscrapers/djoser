1fe6f734f7d184e429ad6cc914400495eea0d0c9
Piotr Szpetkowski <piotr.szpetkowski@pyquest.space>
2018-01-22 16:13:22 +0100

    Replace BasePipelineViewSet with run_pipeline func

--- djoser/views.py
+++ djoser/views.py
@@ -8,81 +8,78 @@ from djoser.conf import settings
 User = get_user_model()
 
 
-class BasePipelineViewSet(viewsets.ViewSet):
-    view_pipeline_adapter = settings.VIEW_PIPELINE_ADAPTER
-
-    def run(self, pipeline, request):
-        return BasePipelineViewSet.view_pipeline_adapter(pipeline, request)
+def run_pipeline(pipeline, request):
+    return settings.VIEW_PIPELINE_ADAPTER(pipeline, request)
 
 
-class UsersViewSet(BasePipelineViewSet):
+class UsersViewSet(viewsets.ViewSet):
     permission_classes = [permissions.AllowAny]
 
     def create(self, request, *args, **kwargs):
         pipeline = pipelines.user_create.Pipeline
-        response_data = self.run(pipeline, request)
+        response_data = run_pipeline(pipeline, request)
         return Response(response_data, status=status.HTTP_201_CREATED)
 
 
-class UserViewSet(BasePipelineViewSet):
+class UserViewSet(viewsets.ViewSet):
     permission_classes = [permissions.IsAuthenticated]
 
     def me(self, request, *args, **kwargs):
         pipeline = pipelines.user_detail.Pipeline
-        response_data = self.run(pipeline, request)
+        response_data = run_pipeline(pipeline, request)
         return Response(response_data, status=status.HTTP_200_OK)
 
     def update(self, request, *args, **kwargs):
         pipeline = pipelines.user_update.Pipeline
-        response_data = self.run(pipeline, request)
+        response_data = run_pipeline(pipeline, request)
         return Response(response_data, status=status.HTTP_200_OK)
 
     def remove(self, request, *args, **kwargs):
         pipeline = pipelines.user_delete.Pipeline
-        self.run(pipeline, request)
+        run_pipeline(pipeline, request)
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-class UserActivateViewSet(BasePipelineViewSet):
+class UserActivateViewSet(viewsets.ViewSet):
     permission_classes = [permissions.AllowAny]
 
     def create(self, request, *args, **kwargs):
         pipeline = pipelines.user_activate.Pipeline
-        self.run(pipeline, request)
+        run_pipeline(pipeline, request)
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-class UsernameUpdateViewSet(BasePipelineViewSet):
+class UsernameUpdateViewSet(viewsets.ViewSet):
     permission_classes = [permissions.IsAuthenticated]
 
     def create(self, request, *args, **kwargs):
         pipeline = pipelines.username_update.Pipeline
-        self.run(pipeline, request)
+        run_pipeline(pipeline, request)
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-class PasswordUpdateViewSet(BasePipelineViewSet):
+class PasswordUpdateViewSet(viewsets.ViewSet):
     permission_classes = [permissions.IsAuthenticated]
 
     def create(self, request, *args, **kwargs):
         pipeline = pipelines.password_update.Pipeline
-        self.run(pipeline, request)
+        run_pipeline(pipeline, request)
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-class PasswordResetViewSet(BasePipelineViewSet):
+class PasswordResetViewSet(viewsets.ViewSet):
     permission_classes = [permissions.AllowAny]
 
     def create(self, request, *args, **kwargs):
         pipeline = pipelines.password_reset.Pipeline
-        self.run(pipeline, request)
+        run_pipeline(pipeline, request)
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-class PasswordResetConfirmViewSet(BasePipelineViewSet):
+class PasswordResetConfirmViewSet(viewsets.ViewSet):
     permission_classes = [permissions.AllowAny]
 
     def create(self, request, *args, **kwargs):
         pipeline = pipelines.password_reset_confirm.Pipeline
-        self.run(pipeline, request)
+        run_pipeline(pipeline, request)
         return Response(status=status.HTTP_204_NO_CONTENT)
