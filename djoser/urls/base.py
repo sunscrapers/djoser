0ba2e9c28f2fa8aa3cc3884a5fca7d0aaef5e4e0
Piotr Szpetkowski <piotr.szpetkowski@pyquest.space>
2018-01-22 12:15:35 +0100

    Update base URLs for new pipeline-based viewsets

--- djoser/urls/base.py
+++ djoser/urls/base.py
@@ -1,48 +1,61 @@
-from django.conf.urls import url, include
+from django.conf.urls import include, url
 from django.contrib.auth import get_user_model
+
+from rest_framework.routers import DefaultRouter, Route
 
 from djoser import views
 
-from rest_framework.routers import DefaultRouter
-
-router = DefaultRouter()
-router.register('users', views.UserViewSet)
-
 User = get_user_model()
 
+
+class OptionalSlashRouter(DefaultRouter):
+    def __init__(self):
+        super(OptionalSlashRouter, self).__init__()
+        self.trailing_slash = '/?'
+
+
+router = OptionalSlashRouter()
+router.routes.append(Route(
+    url=r'^user{trailing_slash}$',
+    mapping={
+        'delete': 'remove',
+        'get': 'me',
+        'put': 'update',
+    },
+    name='{basename}-instance',
+    initkwargs={'suffix': 'Instance'}
+))
+router.register(
+    r'user/{}'.format(User.USERNAME_FIELD),
+    views.UsernameUpdateViewSet,
+    base_name='username-update',
+)
+router.register(
+    r'user/password',
+    views.PasswordUpdateViewSet,
+    base_name='password-reset',
+)
+router.register(
+    r'password/reset',
+    views.PasswordResetConfirmViewSet,
+    base_name='password-reset-confirm',
+)
+router.register(
+    r'user',
+    views.UserViewSet,
+    base_name='user',
+)
+router.register(
+    r'users',
+    views.UsersViewSet,
+    base_name='users',
+)
+router.register(
+    r'users/activate',
+    views.UserActivateViewSet,
+    base_name='user-activate',
+)
+
 urlpatterns = [
-    url(r'^me/?$', views.UserView.as_view(), name='user'),
-    url(
-        r'^users/create/?$',
-        views.UserCreateView.as_view(),
-        name='user-create'
-    ),
-    url(
-        r'^users/delete/?$',
-        views.UserDeleteView.as_view(),
-        name='user-delete'
-    ),
-    url(
-        r'^users/activate/?$',
-        views.ActivationView.as_view(),
-        name='user-activate'
-    ),
-    url(
-        r'^{0}/?$'.format(User.USERNAME_FIELD),
-        views.SetUsernameView.as_view(),
-        name='set_username'
-    ),
-    url(r'^password/?$', views.SetPasswordView.as_view(), name='set_password'),
-    url(
-        r'^password/reset/?$',
-        views.PasswordResetView.as_view(),
-        name='password_reset'
-    ),
-    url(
-        r'^password/reset/confirm/?$',
-        views.PasswordResetConfirmView.as_view(),
-        name='password_reset_confirm'
-    ),
-    url(r'^$', views.RootView.as_view(), name='root'),
     url(r'^', include(router.urls)),
 ]
