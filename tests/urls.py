1c4d8148eea5c6494742801f133cba2a3fb228ed
Piotr Szpetkowski <piotr.szpetkowski@pyquest.space>
2018-02-04 01:21:21 +0100

    Update test URLs to use DjoserRouter

--- tests/urls.py
+++ tests/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url, include
 
+from djoser.urls import DjoserRouter
+
+
+router = DjoserRouter(include_token_urls=True)
 
 urlpatterns = (
-    url(r'^', include('djoser.urls.base')),
+    url(r'^', include(router.urls)),
 )
