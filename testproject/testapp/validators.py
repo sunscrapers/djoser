392d73c8e51ae158b4b595b9629cd5d902e77172
Piotr Szpetkowski <piotr.szpetkowski@pyquest.space>
2018-02-04 09:42:28 +0100

    Remove old testproject

--- testproject/testapp/validators.py
+++
@@ -1,7 +0,0 @@
-from django.core.exceptions import ValidationError
-
-
-class Is666(object):
-    def validate(self, password, *args, **kwargs):
-        if password == '666':
-            raise ValidationError("Password 666 is not allowed.")
--- testproject/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url, include
-
-
-urlpatterns = (
-    url(r'^auth/', include('djoser.urls.base')),
-    url(r'^auth/', include('djoser.urls.authtoken')),
-    url(r'^auth/', include('djoser.urls.jwt')),
-    url(r'^auth/', include('djoser.social.urls')),
-)
