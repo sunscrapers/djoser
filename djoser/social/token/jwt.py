f6a5708aafcafdb2fbca7eff1119939d0c2d53d4
Piotr Szpetkowski <piotr.szpetkowski@pyquest.space>
2018-02-04 09:42:12 +0100

    Remove old social auth

--- djoser/social/token/jwt.py
+++
@@ -1,9 +0,0 @@
-class TokenStrategy:
-    @classmethod
-    def obtain(cls, user):
-        from rest_framework_jwt.settings import api_settings
-        payload = api_settings.JWT_PAYLOAD_HANDLER(user)
-        return {
-            'token': api_settings.JWT_ENCODE_HANDLER(payload),
-            'user': user
-        }
