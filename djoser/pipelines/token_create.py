76287efaacbde1ac273af4980bcf94003b19f8b0
Piotr Szpetkowski <piotr.szpetkowski@pyquest.space>
2018-02-04 01:14:43 +0100

    Add serialize_instance step to token_create

--- djoser/pipelines/token_create.py
+++ djoser/pipelines/token_create.py
@@ -20,9 +20,15 @@ def perform(serializer, **kwargs):
         raise exceptions.ValidationError(constants.TOKEN_MODEL_NONE_ERROR)
 
     token, _ = settings.TOKEN_MODEL.objects.get_or_create(user=user)
-    return {'user': user}
+    return {'token': token, 'user': user}
 
 
 def signal(request, user, **kwargs):
     signals.token_created.send(sender=None, user=user, request=request)
     user_logged_in.send(sender=None, user=user, request=request)
+
+
+def serialize_instance(token, **kwargs):
+    serializer_class = settings.SERIALIZERS['token']
+    serializer = serializer_class(token)
+    return {'response_data': serializer.data}
