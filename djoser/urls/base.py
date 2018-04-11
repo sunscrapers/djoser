cf90dcbd23c458f70c767a31903a0843b46fc368
Andrew Meares <andrew.meares@perspica.city>
2018-04-11 10:27:45 +0200

    Fix regex to python format

--- djoser/urls/base.py
+++ djoser/urls/base.py
@@ -7,35 +7,35 @@ User = get_user_model()
 
 
 urlpatterns = [
-    url(r'^me[/]$', views.UserView.as_view(), name='user'),
+    url(r'^me/?$', views.UserView.as_view(), name='user'),
     url(
-        r'^users/create[/]$',
+        r'^users/create/?$',
         views.UserCreateView.as_view(),
         name='user-create'
     ),
     url(
-        r'^users/delete[/]$',
+        r'^users/delete/?$',
         views.UserDeleteView.as_view(),
         name='user-delete'
     ),
     url(
-        r'^users/activate[/]$',
+        r'^users/activate/?$',
         views.ActivationView.as_view(),
         name='user-activate'
     ),
     url(
-        r'^{0}[/]$'.format(User.USERNAME_FIELD),
+        r'^{0}/?$'.format(User.USERNAME_FIELD),
         views.SetUsernameView.as_view(),
         name='set_username'
     ),
-    url(r'^password[/]$', views.SetPasswordView.as_view(), name='set_password'),
+    url(r'^password/?$', views.SetPasswordView.as_view(), name='set_password'),
     url(
-        r'^password/reset[/]$',
+        r'^password/reset/?$',
         views.PasswordResetView.as_view(),
         name='password_reset'
     ),
     url(
-        r'^password/reset/confirm[/]$',
+        r'^password/reset/confirm/?$',
         views.PasswordResetConfirmView.as_view(),
         name='password_reset_confirm'
     ),
