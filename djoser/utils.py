ded0d2ced823deeabf860abd9ec5120165ed7fde
Nache <nache.nache@gmail.com>
2019-02-26 21:06:05 +0100

    Add **kwargs to ActionViewmixin.post() handler
    Details: #359

--- djoser/utils.py
+++ djoser/utils.py
@@ -32,7 +32,7 @@ def logout_user(request):
 
 
 class ActionViewMixin(object):
-    def post(self, request):
+    def post(self, request, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         return self._action(serializer)
