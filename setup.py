c911202ff20b63b02aee492140e8493b8925ab54
Dominik Kozaczko <dominik@kozaczko.info>
2019-05-25 10:51:51 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def get_packages(package):
 
 setup(
     name='djoser',
-    version='1.6.0',
+    version='1.7.0',
     packages=get_packages('djoser'),
     license='MIT',
     author='SUNSCRAPERS',
