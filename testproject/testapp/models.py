5ca82b5c2d98643dc300a224ac683ca46917d560
Pixsel1 <pmlynarek1@gmail.com>
2019-06-03 08:26:00 +0200

    Fix pep8.

--- testproject/testapp/models.py
+++ testproject/testapp/models.py
@@ -6,12 +6,18 @@ from django.db import models
 class CustomUserManager(BaseUserManager):
     use_in_migrations = True
 
-    def create_user(self, custom_username, custom_email=None, password=None, **extra_fields):
+    def create_user(
+        self, custom_username, custom_email=None, password=None, **extra_fields
+    ):
         if not custom_username:
             raise ValueError('The given custom_username must be set')
         email = self.normalize_email(custom_email)
         username = self.model.normalize_username(custom_username)
-        user = self.model(custom_username=username, custom_email=email, **extra_fields)
+        user = self.model(
+            custom_username=username,
+            custom_email=email,
+            **extra_fields
+        )
         user.set_password(password)
         user.save(using=self._db)
         return user
@@ -27,4 +33,3 @@ class CustomUser(AbstractBaseUser):
     EMAIL_FIELD = 'custom_email'
     USERNAME_FIELD = 'custom_username'
     REQUIRED_FIELDS = ['custom_email', 'custom_required_field']
-
