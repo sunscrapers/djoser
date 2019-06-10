b8c50043595fed64ebbaa7da9ef2e4c01bc334c3
Igor Kavzov <igor.kavzov@gmail.com>
2019-06-10 19:55:25 +0300

    Validator for test custom validators

--- testproject/testapp/validators.py
+++ testproject/testapp/validators.py
@@ -5,3 +5,9 @@ class Is666(object):
     def validate(self, password, *args, **kwargs):
         if password == '666':
             raise ValidationError("Password 666 is not allowed.", code='no666')
+
+
+class Is666666(object):
+    def validate(self, password, *args, **kwargs):
+        if password == '666666':
+            raise ValidationError("Password 666666 is extremely not allowed.", code='no666666')
