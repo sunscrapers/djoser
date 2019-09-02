195dec813d1c124d7f23570f642f18ce5f15da5b
Olivier Roux <oroux@olfeo.com>
2019-09-02 17:52:12 +0200

    Fix current_password being ignored by the set_username endpoint

--- djoser/serializers.py
+++ djoser/serializers.py
@@ -329,9 +329,12 @@ class UserDeleteSerializer(CurrentPasswordSerializer):
     pass
 
 
-class SetUsernameSerializer(UsernameSerializer, CurrentPasswordSerializer):
-    pass
+class SetUsernameSerializer(UsernameSerializer,
+                            CurrentPasswordSerializer):
+    class Meta:
+        model = User
+        fields = (settings.LOGIN_FIELD, 'current_password')
 
 
-class SetUsernameRetypeSerializer(UsernameRetypeSerializer, CurrentPasswordSerializer):
+class SetUsernameRetypeSerializer(SetUsernameSerializer, UsernameRetypeSerializer):
     pass
