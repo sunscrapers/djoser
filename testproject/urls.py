0e138fb26cf7022e257ba56577c4b84dfb996e64
Tomasz Wójcik <tomwojcik@users.noreply.github.com>
2020-09-03 14:19:39 +0200

    replace url with re_path due to django4.0 warnings

--- testproject/urls.py
+++ testproject/urls.py
@@ -1,11 +1,11 @@
-from django.conf.urls import include, url
+from django.urls import include, re_path
 from django.views.generic import TemplateView
 
 urlpatterns = (
-    url(r"^auth/", include("djoser.urls.base")),
-    url(r"^auth/", include("djoser.urls.authtoken")),
-    url(r"^auth/", include("djoser.urls.jwt")),
-    url(r"^auth/", include("djoser.social.urls")),
-    url(r"^webauthn/", include("djoser.webauthn.urls")),
-    url(r"^webauthn-example/$", TemplateView.as_view(template_name="webauthn.html")),
+    re_path(r"^auth/", include("djoser.urls.base")),
+    re_path(r"^auth/", include("djoser.urls.authtoken")),
+    re_path(r"^auth/", include("djoser.urls.jwt")),
+    re_path(r"^auth/", include("djoser.social.urls")),
+    re_path(r"^webauthn/", include("djoser.webauthn.urls")),
+    re_path(r"^webauthn-example/$", TemplateView.as_view(template_name="webauthn.html")),
 )
