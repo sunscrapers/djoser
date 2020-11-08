1d542efa9179ebd69bc7b33d355c9873a61fe73b
zach <zach@zach-macbook.local>
2020-11-08 10:26:24 -0600

    syntax update

--- djoser/views.py
+++ djoser/views.py
@@ -148,8 +148,9 @@ class UserViewSet(viewsets.ModelViewSet):
     def perform_update(self, serializer):
         super().perform_update(serializer)
         user = serializer.instance
+        email = serializer.validated_data.get("email", None)
         # should we send activation email after update?
-        if settings.SEND_ACTIVATION_EMAIL:
+        if settings.SEND_ACTIVATION_EMAIL and email is not None:
             context = {"user": user}
             to = [get_user_email(user)]
             settings.EMAIL.activation(self.request, context).send(to)
