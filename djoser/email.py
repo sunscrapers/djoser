aaa1098070c75de6aede4c81bafc1eb704a3a42a
Michael Might <64410120+mikemight@users.noreply.github.com>
2021-01-19 16:18:34 +0200

    removed code duplication by adding a base class

--- djoser/email.py
+++ djoser/email.py
@@ -5,36 +5,32 @@ from djoser import utils
 from djoser.conf import settings
 
 
-class ActivationEmail(BaseEmailMessage):
-    template_name = "email/activation.html"
-
+class MessageWithUniqueLink(BaseEmailMessage):
+    address = None
+    
     def get_context_data(self):
-        # ActivationEmail can be deleted
+        # Email can be deleted
         context = super().get_context_data()
 
         user = context.get("user")
         context["uid"] = utils.encode_uid(user.pk)
         context["token"] = default_token_generator.make_token(user)
-        context["url"] = settings.ACTIVATION_URL.format(**context)
-        return context
+        context["url"] = self.address.format(**context)
+        return context 
+
+
+class ActivationEmail(MessageWithUniqueLink):
+    address = settings.ACTIVATION_URL
+    template_name = "email/activation.html"
 
 
 class ConfirmationEmail(BaseEmailMessage):
     template_name = "email/confirmation.html"
 
 
-class PasswordResetEmail(BaseEmailMessage):
+class PasswordResetEmail(MessageWithUniqueLink):
+    address = settings.PASSWORD_RESET_CONFIRM_URL
     template_name = "email/password_reset.html"
-
-    def get_context_data(self):
-        # PasswordResetEmail can be deleted
-        context = super().get_context_data()
-
-        user = context.get("user")
-        context["uid"] = utils.encode_uid(user.pk)
-        context["token"] = default_token_generator.make_token(user)
-        context["url"] = settings.PASSWORD_RESET_CONFIRM_URL.format(**context)
-        return context
 
 
 class PasswordChangedConfirmationEmail(BaseEmailMessage):
@@ -45,14 +41,6 @@ class UsernameChangedConfirmationEmail(BaseEmailMessage):
     template_name = "email/username_changed_confirmation.html"
 
 
-class UsernameResetEmail(BaseEmailMessage):
+class UsernameResetEmail(MessageWithUniqueLink):
+    address = settings.USERNAME_RESET_CONFIRM_URL
     template_name = "email/username_reset.html"
-
-    def get_context_data(self):
-        context = super().get_context_data()
-
-        user = context.get("user")
-        context["uid"] = utils.encode_uid(user.pk)
-        context["token"] = default_token_generator.make_token(user)
-        context["url"] = settings.USERNAME_RESET_CONFIRM_URL.format(**context)
-        return context
