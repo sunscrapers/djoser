1e479708a45260df0d2067a92498f1ac8e180760
Mike <80258656+aboryone@users.noreply.github.com>
2021-07-03 21:11:49 +0300

    removed duplicate code

--- djoser/email.py
+++ djoser/email.py
@@ -5,36 +5,32 @@ from djoser import utils
 from djoser.conf import settings
 
 
-class ActivationEmail(BaseEmailMessage):
-    template_name = "email/activation.html"
-
+class EmailWithUniqueLink(BaseEmailMessage):
+    address = None
+    
     def get_context_data(self):
-        # ActivationEmail can be deleted
+        # Email can be deleted
         context = super().get_context_data()
 
         user = context.get("user")
         context["uid"] = utils.encode_uid(user.pk)
         context["token"] = default_token_generator.make_token(user)
-        context["url"] = settings.ACTIVATION_URL.format(**context)
+        context["url"] = self.address.format(**context)
         return context
+
+
+class ActivationEmail(EmailWithUniqueLink):
+    template_name = "email/activation.html"
+    address = settings.ACTIVATION_URL
 
 
 class ConfirmationEmail(BaseEmailMessage):
     template_name = "email/confirmation.html"
 
 
-class PasswordResetEmail(BaseEmailMessage):
+class PasswordResetEmail(EmailWithUniqueLink):
     template_name = "email/password_reset.html"
-
-    def get_context_data(self):
-        # PasswordResetEmail can be deleted
-        context = super().get_context_data()
-
-        user = context.get("user")
-        context["uid"] = utils.encode_uid(user.pk)
-        context["token"] = default_token_generator.make_token(user)
-        context["url"] = settings.PASSWORD_RESET_CONFIRM_URL.format(**context)
-        return context
+    address = settings.PASSWORD_RESET_CONFIRM_URL
 
 
 class PasswordChangedConfirmationEmail(BaseEmailMessage):
@@ -45,14 +41,6 @@ class UsernameChangedConfirmationEmail(BaseEmailMessage):
     template_name = "email/username_changed_confirmation.html"
 
 
-class UsernameResetEmail(BaseEmailMessage):
+class UsernameResetEmail(EmailWithUniqueLink):
     template_name = "email/username_reset.html"
-
-    def get_context_data(self):
-        context = super().get_context_data()
-
-        user = context.get("user")
-        context["uid"] = utils.encode_uid(user.pk)
-        context["token"] = default_token_generator.make_token(user)
-        context["url"] = settings.USERNAME_RESET_CONFIRM_URL.format(**context)
-        return context
+    address = settings.USERNAME_RESET_CONFIRM_URL
