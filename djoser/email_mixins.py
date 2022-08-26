37b4ff66f6810de597c1eef953addef5d8d69a5a
mojtabaakbari221b <mojtaba.akbari.221b@gmail.com>
2022-08-26 13:02:43 +0430

    add base class for adding context to email instance

--- djoser/email_mixins.py
+++ djoser/email_mixins.py
@@ -1,3 +1,18 @@
-class CommonContextEmail():
-    def add_common_context(self):
-        pass
+from django.contrib.auth.tokens import default_token_generator
+
+from djoser import utils
+
+
+class EmailContext:
+
+    def get_context_data(self):
+        context = super().get_context_data()
+        context = self.add_common_context(context)
+        return context
+    
+    def add_common_context(self, context):
+        user = context.get("user")
+        context["uid"] = utils.encode_uid(user.pk)
+        context["token"] = default_token_generator.make_token(user)
+        context["url"] = self.url.format(**context)
+        return context
