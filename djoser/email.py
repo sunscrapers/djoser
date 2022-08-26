b54153bce39fbc26fa8e9934300a3abeb0c08277
mojtabaakbari221b <mojtaba.akbari.221b@gmail.com>
2022-08-26 13:03:42 +0430

    extend send email from email_mixin duo to avoid writing extra code

--- djoser/email.py
+++ djoser/email.py
@@ -1,40 +1,22 @@
-from django.contrib.auth.tokens import default_token_generator
 from templated_mail.mail import BaseEmailMessage
 
-from djoser import utils
 from djoser.conf import settings
 
+from .email_mixins import EmailContext
 
-class ActivationEmail(BaseEmailMessage):
+
+class ActivationEmail(EmailContext, BaseEmailMessage):
     template_name = "email/activation.html"
-
-    def get_context_data(self):
-        # ActivationEmail can be deleted
-        context = super().get_context_data()
-
-        user = context.get("user")
-        context["uid"] = utils.encode_uid(user.pk)
-        context["token"] = default_token_generator.make_token(user)
-        context["url"] = settings.ACTIVATION_URL.format(**context)
-        return context
+    url = settings.ACTIVATION_URL
 
 
 class ConfirmationEmail(BaseEmailMessage):
     template_name = "email/confirmation.html"
 
 
-class PasswordResetEmail(BaseEmailMessage):
+class PasswordResetEmail(EmailContext, BaseEmailMessage):
     template_name = "email/password_reset.html"
-
-    def get_context_data(self):
-        # PasswordResetEmail can be deleted
-        context = super().get_context_data()
-
-        user = context.get("user")
-        context["uid"] = utils.encode_uid(user.pk)
-        context["token"] = default_token_generator.make_token(user)
-        context["url"] = settings.PASSWORD_RESET_CONFIRM_URL.format(**context)
-        return context
+    url = settings.PASSWORD_RESET_CONFIRM_URL
 
 
 class PasswordChangedConfirmationEmail(BaseEmailMessage):
@@ -45,14 +27,6 @@ class UsernameChangedConfirmationEmail(BaseEmailMessage):
     template_name = "email/username_changed_confirmation.html"
 
 
-class UsernameResetEmail(BaseEmailMessage):
+class UsernameResetEmail(EmailContext, BaseEmailMessage):
     template_name = "email/username_reset.html"
-
-    def get_context_data(self):
-        context = super().get_context_data()
-
-        user = context.get("user")
-        context["uid"] = utils.encode_uid(user.pk)
-        context["token"] = default_token_generator.make_token(user)
-        context["url"] = settings.USERNAME_RESET_CONFIRM_URL.format(**context)
-        return context
+    url = settings.USERNAME_RESET_CONFIRM_URL
