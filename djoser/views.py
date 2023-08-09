85d2765a34da42597955e410ea4141412bc9ddf6
Andrey Shpak <ashpak@ashpak.ru>
2023-08-09 11:38:32 +0300

    Update TokenCreateView mro

--- djoser/views.py
+++ djoser/views.py
@@ -13,7 +13,7 @@ from djoser.conf import settings
 User = get_user_model()
 
 
-class TokenCreateView(utils.ActionViewMixin, generics.GenericAPIView):
+class TokenCreateView(generics.GenericAPIView):
     """Use this endpoint to obtain user authentication token."""
 
     serializer_class = settings.SERIALIZERS.token_create
@@ -25,6 +25,11 @@ class TokenCreateView(utils.ActionViewMixin, generics.GenericAPIView):
         return Response(
             data=token_serializer_class(token).data, status=status.HTTP_200_OK
         )
+
+    def post(self, request, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return self._action(serializer)
 
 
 class TokenDestroyView(views.APIView):
