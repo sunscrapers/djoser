81458a52275dba2d1153b6a537875148fc25835c
Tom Wojcik <me@tomwojcik.com>
2023-11-09 11:46:14 +0100

    Revert "replace re_path with path"
    This reverts commit 16f406e9

--- djoser/urls/authtoken.py
+++ djoser/urls/authtoken.py
@@ -1,8 +1,8 @@
-from django.urls import path
+from django.urls import re_path
 
 from djoser import views
 
 urlpatterns = [
-    path("token/login/", views.TokenCreateView.as_view(), name="login"),
-    path("token/logout/", views.TokenDestroyView.as_view(), name="logout"),
+    re_path(r"^token/login/?$", views.TokenCreateView.as_view(), name="login"),
+    re_path(r"^token/logout/?$", views.TokenDestroyView.as_view(), name="logout"),
 ]
