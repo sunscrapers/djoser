29aead36bbc480b06184808c447963f8213ec42f
Tom Wojcik <me@tomwojcik.com>
2024-05-04 15:18:15 +0200

    drop router from the new UserViewSet in urls

--- djoser/urls/base.py
+++ djoser/urls/base.py
@@ -1,5 +1,5 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
-
 
 from djoser.views import UserViewSet as OldUserViewSet
 from djoser.views.user.user import UserViewSet
@@ -7,15 +7,24 @@ from djoser.views.user.user import UserViewSet
 old_router = DefaultRouter()
 old_router.register("users-old", OldUserViewSet)
 
-router = DefaultRouter()
-router.register("users", UserViewSet)
-
-# User = get_user_model()
-
-# users_url = path("users/", ListCreateUserView.as_view(), name="user-list")
+user_list = path(
+    "users/", UserViewSet.as_view({"get": "list", "post": "create"}), name="user-list"
+)
+user_detail = path(
+    f"users/<{UserViewSet.lookup_field}>/",
+    UserViewSet.as_view(
+        {
+            "get": "retrieve",
+            "put": "update",
+            "patch": "partial_update",
+            "delete": "destroy",
+        }
+    ),
+    name="user-detail",
+)
 
 urlpatterns = [
-    # users_url,
     *old_router.urls,
-    *router.urls,
+    user_list,
+    user_detail,
 ]
