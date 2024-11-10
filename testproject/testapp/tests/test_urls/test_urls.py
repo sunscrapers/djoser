9deaa635c8521e2c030b810c4753da0f3a358c1f
Tomasz Wójcik <tomwojcik@users.noreply.github.com>
2024-11-10 14:06:08 +0100

    add method names to test urls test (#856)

--- testproject/testapp/tests/test_urls/test_urls.py
+++ testproject/testapp/tests/test_urls/test_urls.py
@@ -1,5 +1,6 @@
 import json
 import pathlib
+from contextlib import suppress
 
 import pytest
 from deepdiff import DeepDiff
@@ -26,7 +27,33 @@ def test_urls_have_not_changed(settings):
             else:
                 pattern_str = prefix + pattern.pattern.regex.pattern
                 name = pattern.name if pattern.name else None
-                urls.append({"pattern": pattern_str, "name": name})
+                allowed_methods = []
+                if hasattr(pattern, "callback"):
+                    view = pattern.callback
+                    if hasattr(view, "http_method_names"):
+                        allowed_methods = view.http_method_names
+                    elif hasattr(view, "actions"):
+                        allowed_methods = list(view.actions.keys())
+                    elif hasattr(
+                        view, "view_class"
+                    ):  # assume all, even though probably not
+                        allowed_methods = view.view_class.http_method_names
+                    else:
+                        raise NotImplementedError(
+                            "Function based views are not supported"
+                        )
+                # head is not present in the CI for some reason...
+                with suppress(ValueError):
+                    i = allowed_methods.index("head")
+                    del allowed_methods[i]
+
+                urls.append(
+                    {
+                        "pattern": pattern_str,
+                        "name": name,
+                        "allowed_methods": allowed_methods,
+                    }
+                )
         return urls
 
     current_urls = sorted(get_all_urls(url_patterns), key=lambda x: x["pattern"])
