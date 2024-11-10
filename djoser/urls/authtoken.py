44ea180c7d52235a1820d40517a8f90855ea3a80
Tom Wojcik <me@tomwojcik.com>
2024-11-10 14:40:16 +0100

    mv re_path to path in jwt and auth token

--- djoser/urls/authtoken.py
+++ djoser/urls/authtoken.py
@@ -1,9 +1,9 @@
-from django.urls import re_path
+from django.urls import path
 
 from djoser.views.token.create import TokenCreateView
 from djoser.views.token.destroy import TokenDestroyView
 
 urlpatterns = [
-    re_path(r"^token/login/?$", TokenCreateView.as_view(), name="login"),
-    re_path(r"^token/logout/?$", TokenDestroyView.as_view(), name="logout"),
+    path("token/login/", TokenCreateView.as_view(), name="login"),
+    path("token/logout/", TokenDestroyView.as_view(), name="logout"),
 ]
