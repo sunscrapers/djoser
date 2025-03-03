555b9724427b577e9060c1e97c882b723821b6e8
Tomasz Wójcik <tomwojcik@users.noreply.github.com>
2025-03-03 20:11:18 +0100

    extract code quality GHA to is own job (#863)
    * extract code quality GHA to is own job
    
    * extract venv cache, refactor test suite to use it and test vs django 5 latest
    
    * add dependabot
    
    * drop python 3.8 support, bump deps, update lock
    
    * lint

--- djoser/permissions.py
+++ djoser/permissions.py
@@ -11,6 +11,6 @@ class CurrentUserOrAdmin(permissions.IsAuthenticated):
 class CurrentUserOrAdminOrReadOnly(permissions.IsAuthenticated):
     def has_object_permission(self, request, view, obj):
         user = request.user
-        if type(obj) == type(user) and obj == user:
+        if type(obj) is type(user) and obj == user:
             return True
         return request.method in SAFE_METHODS or user.is_staff
